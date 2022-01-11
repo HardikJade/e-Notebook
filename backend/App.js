8d32f9839f5681ac1727288a2a42c32b567ff414
Hardik Jade <hardikjade2808@gmail.com>
2022-01-11 20:29:15 +0530

    Final Commit

--- backend/App.js
+++ backend/App.js
@@ -14,6 +14,8 @@ app.use(express.json())
 // All Available Routes
 app.get('/', (req, res) => {res.send('Hello World!')})
 app.use('/api/auth',require("./routes/auth"));
+app.get("*",(request,response)=>{response.status(400).send("PAGE NOT FOUND")})
+app.post("*",(request,response)=>{response.status(400).send("PAGE NOT FOUND")})
 // app.use('/api/notes',require("./routes/notes"));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
